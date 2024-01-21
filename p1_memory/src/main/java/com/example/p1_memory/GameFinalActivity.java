8a06742bd3016129187fa56beb7259a3c1487355
nonya biznes <slavs12@yahoo.com>
2024-01-21 03:06:09 -0700

    Major Code in now to tweak

--- p1_memory/src/main/java/com/example/p1_memory/GameFinalActivity.java
+++ p1_memory/src/main/java/com/example/p1_memory/GameFinalActivity.java
@@ -3,7 +3,6 @@ package com.example.p1_memory;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.p1_memory.databinding.ActivityGameFinalBinding;
--- p1_memory/src/main/java/com/example/p1_memory/ViewModel.java
+++ p1_memory/src/main/java/com/example/p1_memory/ViewModel.java
@@ -1,7 +1,5 @@
 package com.example.p1_memory;
 
-import android.content.Intent;
-
 public class ViewModel extends androidx.lifecycle.ViewModel {
     public int finalScore = 5;
 
